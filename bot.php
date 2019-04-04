fadb0871578b29c1493ba0c289e32a7e36f140bf
petro <36398735+PetroSutch@users.noreply.github.com>
2019-04-04 11:30:24 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -70,44 +70,45 @@ if (strpos($messageText, 'wea') !== false) {
   $xbodycontent = pq('#blockDays > .tabs')->find('.main');
   $i=0;
   foreach ($xbodycontent as $res) {
-    $pqres=pq($res);
-    $day = $pqres->find('.day-link')->text();//день
-    $date = $pqres->find('.date')->text();//дата
-    $icon = $pqres->find('.weatherIco')->attr('title');//надпись на картинке
-    $iconimg = $pqres->find('.weatherImg')->html();//надпись на картинке
-    $temperature = $pqres->find('.temperature')->text();// Температура
-    //проверяем что а погода чтобы определить какой смайлик отправить для визуализации
-    $emoji = '';
-  if ($icon == 'Сплошная облачность, небольшой снег') {
-    $emoji = hex2bin('F09F8CA809'); //hex2bin('f09f9880') //представление смайла https://apps.timwhitlock.info/emoji/tables/unicode
-  }
-  if ($icon == 'Сплошная облачность, сильный снег') {
-    $emoji = hex2bin('F09F8CA809');
-  }
-   if ($icon == 'Сплошная облачность, снег') {
-    $emoji = hex2bin('E29B84');
-  }
-  if ($icon == 'Сплошная облачность') {
-    $emoji = hex2bin('E29881');
-  }
-  if ($icon == 'Облачно с прояснениями, мокрый снег') {
-    $emoji =hex2bin('F09F8CA609');
-  }
-  if ($icon == 'Облачно с прояснениями') {
-    $emoji =hex2bin('F09F8CA409');
-  }
-  if ($icon == 'Облачно с прояснениями, небольшой снег') {
-    $emoji =hex2bin('E29D8409');
-  }
- // оправляем в канал результат
-  $response['message'] = ['text'=>$day.' '.$date.chr(10).' '.$temperature.chr(10).' '.$emoji.' '.$icon];
-  sendMsg();
-  $i++;
-  //requestToTelegram($content);
-  if($i == 3){
-     break;
-  }
-}
+	    $pqres=pq($res);
+	    $day = $pqres->find('.day-link')->text();//день
+	    $date = $pqres->find('.date')->text();//дата
+	    $icon = $pqres->find('.weatherIco')->attr('title');//надпись на картинке
+	    $iconimg = $pqres->find('.weatherImg')->html();//надпись на картинке
+	    $temperature = $pqres->find('.temperature')->text();// Температура
+	    //проверяем что а погода чтобы определить какой смайлик отправить для визуализации
+	    $emoji = '';
+	  if ($icon == 'Сплошная облачность, небольшой снег') {
+	    $emoji = hex2bin('F09F8CA809'); //hex2bin('f09f9880') //представление смайла https://apps.timwhitlock.info/emoji/tables/unicode
+	  }
+	  if ($icon == 'Сплошная облачность, сильный снег') {
+	    $emoji = hex2bin('F09F8CA809');
+	  }
+	   if ($icon == 'Сплошная облачность, снег') {
+	    $emoji = hex2bin('E29B84');
+	  }
+	  if ($icon == 'Сплошная облачность') {
+	    $emoji = hex2bin('E29881');
+	  }
+	  if ($icon == 'Облачно с прояснениями, мокрый снег') {
+	    $emoji =hex2bin('F09F8CA609');
+	  }
+	  if ($icon == 'Облачно с прояснениями') {
+	    $emoji =hex2bin('F09F8CA409');
+	  }
+	  if ($icon == 'Облачно с прояснениями, небольшой снег') {
+	    $emoji =hex2bin('E29D8409');
+	  }
+	 // оправляем в канал результат
+	  $res = $day.' '.$date.chr(10).' '.$temperature.chr(10).' '.$emoji.' '.$icon;
+	  $response['message'] = ['text'=>$res];
+	  sendMsg();
+	  $i++;
+	  //requestToTelegram($content);
+	  if($i == 2){
+	     break;
+	  }
+	}
   return false;
   $response['message'] = ['text'=>$answer];
 }
