e24e103c3eea399cee7ca880af6d0e2756aaf2f3
petro <36398735+PetroSutch@users.noreply.github.com>
2019-04-04 11:05:36 +0300

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 $messageText = $_GET['q'];
 var_dump(strpos($messageText, 'wea') !== false);
 if (strpos($messageText, 'wea') !== false) {
-  $option = explode(" ", $text); // Разбивает строку с помощью разделителя $option[1]-Киев,$option[2] = число 3
+  $option = explode(" ", $messageText); // Разбивает строку с помощью разделителя $option[1]-Киев,$option[2] = число 3
   require ('phpQuery.php');  // библиотека
   $url = 'https://sinoptik.ua/погода-'.$option[1].'/10-дней';
 
