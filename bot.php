bb57defb76da84de0ae860611126d8c68b05efde
petro <36398735+PetroSutch@users.noreply.github.com>
2019-04-04 09:18:33 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,25 +26,18 @@ if($messageText == "blog"){
      $answer = ["attachment"=>[
       "type"=>"template",
       "payload"=>[
-        "template_type"=>"generic",
-        "elements"=>[
+        "template_type"=>"button",
+        "text"=>"What do you want to do next?",
+        "buttons"=>[
           [
-            "title"=>"Welcome to Peter\'s Hats",
-            "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-            "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
-            "subtitle"=>"We\'ve got the right hat for everyone.",
-            "buttons"=>[
-              [
-                "type"=>"web_url",
-                "url"=>"https://petersfancybrownhats.com",
-                "title"=>"View Website"
-              ],
-              [
-                "type"=>"postback",
-                "title"=>"Start Chatting",
-                "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
-              ]              
-            ]
+            "type"=>"web_url",
+            "url"=>"https://petersapparel.parseapp.com",
+            "title"=>"Show Website"
+          ],
+          [
+            "type"=>"postback",
+            "title"=>"Start Chatting",
+            "payload"=>"USER_DEFINED_PAYLOAD"
           ]
         ]
       ]
