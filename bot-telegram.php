2d732d269f0cb22d99bf92fe484da2f01137f0a5
petro <36398735+PetroSutch@users.noreply.github.com>
2019-04-08 11:16:08 +0300

    Update bot-telegram.php

--- bot-telegram.php
+++ bot-telegram.php
@@ -1,3 +1,18 @@
 <?php
+$token = "640430644:AAFOS1a9T-mC4WV2uG4ez99xQ5v7dckdwQk";
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Hello!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
 
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Commands:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
 ?>
