97b85e5f68fe85a35f00882ab36380d18cf95b05
petro <36398735+PetroSutch@users.noreply.github.com>
2019-04-13 18:47:38 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,6 +9,7 @@ if ($_REQUEST['hub_verify_token'] == $hubVerifyToken) {
   echo $_REQUEST['hub_challenge'];
   exit;
 }
+persistantMenu();
 
 // handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
